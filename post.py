f284b89d4b90aacc416537ff0603e14f2c621373
SlayerLeon <lev.volkov.98@gmail.com>
2020-12-21 19:46:38 +0500

    This version 1.0 programm Generator For

--- post.py
+++ post.py
@@ -1,7 +1,19 @@
-##!C:\Users\User\AppData\Local\Programs\Python\Python38.exe 
+'''
+Для адекватного распознавания:
++ кодировки utf-8
++ скрипта unix-системами
++ для случаев работы пользователя в виртуальном окружении
++ для определения версии, под которой был разработан данный скрипт
+'''
+##! /usr/local/bin/python
 # Python 3.8.6
-##!\usr\bin\env
+# -*- coding: utf-8 -*-
+##! /usr/bin/env python3
 
+# Импортируем нужные нам библиотеки
+
+import sys
+import time
 from selenium import *
 import random
 import subprocess
@@ -13,50 +25,65 @@ from selenium.common.exceptions import WebDriverException as WDE
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import InvalidElementStateException as IESE
 from setuptools import *
+#import threading
+import setuptools
+from pyfiglet import Figlet
+from colorama import Fore, Back, Style
+
+# Просто ляпота :)
 
 Author = 'SlayerLeon'
+__name__ = 'Generator For'
+f = Figlet(font='slant')
+r0 = print(f.renderText(Author))
+r1 = print(f.renderText(__name__))
+
+
 
 print('Generator of links for Yandex.Disk and Pastebin.com')
 
-'''
-Для скрытия работы Selenium в браузере и CommandPrompt Selenium
-На данный момент скрывает только работу в браузере
-'''
-##driver = webdriver.Chrome('C:\\Users\\User\\Desktop\\chromedriver.exe')
+
+# Для скрытия работы Selenium в браузере и ссылка на chromedriver для работы selenium в Chrome Browser
+
+## driver = webdriver.Chrome('C:\\Users\\User\\Desktop\\chromedriver.exe')
 options = Options()
 options.add_argument("headless")
 options.headless = True
 options.add_argument('--disable-gpu')
-driver = webdriver.Chrome('C:\\Users\\User\\Desktop\\chromedriver.exe', options=options)
+driver = webdriver.Chrome('C:\\Users\\Slayer\\Desktop\\Github\\generator_for_\\chromedriver.exe', options=options)
 
-'''
-Для возможности непрерывной работы  : Желаете ли Вы продолжить работу или нет?
-'''
-##
-##if (query == 'N') or (query == 'not') or (query == 'n') or (query == 'Not') or (query == 'no') or (query == 'No') or (query == 'Нет') or (query == 'нет') or (query == 'не') or (query == 'Не') or (query == 'Да') or (query == 'да') or (query == 'Y') or (query == 'y') or (query == 'Yes') or (query == 'yes'):
-##    while (query == 'N') or (query == 'not') or (query == 'n') or (query == 'Not') or (query == 'no') or (query == 'No') or (query == 'Нет') or (query == 'нет') or (query == 'не') or (query == 'Не'):
-global query
-def main0():
-    query = input(str('If you want close is program, then enter Y/y, otherwise N/n? \n'))
+# ТЕЛО СКРИПТА
+
+# def main(x, event_for_wait, event_for_set):
+query = input(str('If you want close is program, then enter Y/y, otherwise N/n? \n'))   # Желаете ли Вы завершить работу с этой программой?
+# event_for_wait.wait() # wait for event
+# event_for_wait.clear() # clean event for future
+# print (x)
+# event_for_set.set() # set event for neighbor thread
+while (query == 'n') or (query == 'N'):
     service = str(input('Select service (pastebin | yandex.disk):'+'\n'))
     if service == 'yandex.disk':
-        loc = 0
-        m = int(input('Please, enter quantity of links:'))
-        while loc != m:
-            #haven't +-/*!&$#?=w@<>
-            chars = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
-            #length = int(input(col +'Length links?'+ '\n'))
-            length = 14
-            random.shuffle(chars)
-            link = ''.join([random.choice(chars) for x in range(length)])
-            res1 = str(f'https://yadi.sk/d/{link}')
-            res2 = str(f'https://yadi.sk/i/{link}')
-            if (res1 != True) or (res2 != True):
+        loc = 0     # переменная для подсчёта строк ссылок
+        # обработчик ошибок при вводе некорректного значения количества ссылок
+        try:
+            m = int(input('Please, enter quantity of links:'))
+        except ValueError:
+            # while
+            print('Please, enter only one cipher or number')
+            m = int(input('Please, enter quantity of links:'))
+        finally:
+            while loc != m:
+                # без последующих символов +-/*!&$#?=w@<> :%\
+                chars = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
+                # length - это длина link
+                # length = int(input('Length links?'+ '\n'))
+                length = 14
                 random.shuffle(chars)
                 link = ''.join([random.choice(chars) for x in range(length)])
                 res1 = str(f'https://yadi.sk/d/{link}')
                 res2 = str(f'https://yadi.sk/i/{link}')
                 def yc_pluse():
+                    ''' '''
                     global loc
                     loc = str(loc)
                     print(loc+' '+'+'+' '+res1+'\n')
@@ -65,6 +92,7 @@ def main0():
                     loc = loc+1
 
                 def yc_minus():
+                    ''' '''
                     global loc
                     loc = str(loc)
                     print(loc+' '+'-'+' '+res1+'\n')
@@ -103,6 +131,7 @@ def main0():
 
     elif service == 'pastebin':
         col = 0
+        # copy construction upper try-except
         n = int(input('Please, enter quantity of links:'))
         while col != int(n):
             chars = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
@@ -110,7 +139,6 @@ def main0():
             random.shuffle(chars)
             link = ''.join([random.choice(chars) for x in range(length)])
             res0 = str(f'https://pastebin.com/{link}')
-
 ##          res1 = str(f'https://pastebin.com/u/{link}'+'\n')
 ##          Generate link on users
             '''Transition on link at value res1 and work with html'''
@@ -150,39 +178,53 @@ def main0():
                         pstb_pluse()
                     else:
                         pstb_minus()
-                else:
-                    print('ERROR : 00/1_NoSuchElement')
                 finally:
                     pass
-                    '''
-                    WRITE SELECTED LINKS IN RESULT
-                    '''
-                    '''Eceptions errors'''
-                    '''
-                    Сделать возможность работы Selenium в скрытом режиме
-                    + до тех пор, пока не будет положительного результата
-                    '''
+            else:
+                print('ERROR : 00/1_NoSuchElement')
+
+                '''
+                WRITE SELECTED LINKS IN RESULT
+                '''
+                '''Eceptions errors'''
+                '''
+                Сделать возможность работы Selenium в скрытом режиме
+                + до тех пор, пока не будет положительного результата
+                '''
     else:
         print('ERROR : 01_ServiceNotFound')
-    
-global query
-while query != (Y or y or n or N):
-    print('ERROR : 02_UnknownValue')
     query = input(str('If you want close is program, then enter Y/y, otherwise N/n? \n'))
 else:
-    if query == (N or n):
-        main0()
-    elif query == (Y or y):
-        exit()
-    else:
-        print('ERROR : 03_UnknownErrorPleaseResetProgramm')
-# else:
-    # print('ERROR : 02_UnknownValue')
-    # exit()
+     if (query == 'y') or (query == 'Y'):
+         sys.exit()
+     else:
+         print('ERROR : 02_UnknownValue')
+         print('Please, reset this programm or wait 15 sec')
+         time.sleep(15)
+         sys.exit()
 
-##        query = input(str('If you want close is program, then enter Y, otherwise N? \n'))
-##
-##        print('Please, enter other value')
-##        query = input(str('If you want close is program, then enter Y, otherwise N? \n'))
-##    else:
-##        exit()
+# #init Event
+# ev1 = threading.Event()
+# ev2 = threading.Event()
+# ev3 = threading.Event()
+# ev4 = threading.Event()
+#
+# #init Thread
+# th1 = threading.Thread(target=main, args=(0, ev1, ev2))
+# th2 = threading.Thread(target=main, args=(1, ev2, ev3))
+# th3 = threading.Thread(target=main, args=(2, ev3, ev4))
+# th4 = threading.Thread(target=main, args=(3, ev4, ev1))
+#
+# # start threads
+# th1.start()
+# th2.start()
+# th3.start()
+# th4.start()
+#
+# ev1.set() # initiate the first event
+#
+# # join threads to the main thread
+# th1.join()
+# th2.join()
+# th3.join()
+# th4.join()
